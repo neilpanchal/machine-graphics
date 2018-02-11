6edd97d553dff0b1ff7bd2465f07722b4c44eca9
Carbon <carbon@Carbon.local>
2018-02-11 13:24:21 -0700

    Added a display constants

--- machine-graphics.h
+++ machine-graphics.h
@@ -5,20 +5,47 @@
 #include <Print.h>
 
 class MG {
+
 protected:
-	int* buffer;
-	int page_cnt;
-	int pin_cs;
-	int pin_dc;
-	int pin_rst;
+
+	// Display properties:
+	static const int chip_enable_level = 0;
+    static const int chip_enable_level = 0;
+    static const int chip_disable_level = 1;
+    static const int post_chip_enable_wait_ns = 20;
+    static const int pre_chip_disable_wait_ns = 10;
+    static const int reset_pulse_width_ms = 100;
+    static const int post_reset_wait_ms = 100;
+    static const int sda_setup_time_ns = 50;
+    static const int sck_pulse_width_ns = 50;
+    static const int sck_clock_hz = 10000000UL;
+    static const int spi_mode = 0;
+    static const int i2c_bus_clock_100kHz = 4;
+    static const int data_setup_time_ns = 40;
+    static const int write_pulse_width_ns = 150;
+    static const int tile_width = 16;
+    static const int tile_hight = 8;
+    static const int default_x_offset = 0;
+    static const int flipmode_x_offset = 0;
+    static const int pixel_width = 128;
+    static const int pixel_height = 64;
+
+	int* buffer; // Use malloc per display memory
+	int page_cnt; // 8 for some reason?
+	int pin_cs; // Chip select
+	int pin_dc; // DC
+	int pin_rst; // Reset
+
+
 
 public:
 
+	// Initialization methods
 	void initDisplay();
 	void clearDisplay();
-
 	void begin();
 
+	// Buffer methods
 	void clearBuffer();
 	void sendBuffer();
 
